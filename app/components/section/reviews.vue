14ff8233345c9c21f523df8be7edc769fe62e15d
magixe <emax.mails@gmail.com>
2026-02-08 03:58:57 +0500

    refactor order and reviews components for improved layout and styling; enhance responsiveness

--- app/components/section/reviews.vue
+++ app/components/section/reviews.vue
@@ -32,12 +32,12 @@ const reviews = <Review[]>[{
 				<template v-for="(item, index) in reviews as Review[]" :key="index">
 					<div class="item flex flex-col justify-start items-start gap-1 w-full">
 						<div class="flex flex-row justify-between items-start gap-2 w-full">
-							<div class="flex flex-row justify-center items-center gap-5 text-gray-600">
-								<span class="uppercase text-[40px]">
-									<UIcon name="i-lucide-circle-user-round" size="32"/>
+							<div class="flex sm:flex-row flex-col sm:justify-center sm:items-center gap-5 text-gray-600">
+								<div class="uppercase sm:text-[20px] md:text-[30px] lg:text-[36px]">
+									<UIcon name="i-lucide-circle-user-round"/>
 									{{ item.name }}
-								</span>
-								<div class="flex flex-row justify-start items-start gap-1">
+								</div>
+								<div class="flex flex-row justify-start items-start gap-1 mb-5">
 									<img height="20" width="20" v-for="n in item.rating" src="/images/svg/star.svg" :alt="`star +${n}`"/>
 								</div>
 							</div>
