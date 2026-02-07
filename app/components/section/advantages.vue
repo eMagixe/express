239b33e39aa7f179714f0e19565bafc9e21f671e
magixe <emax.mails@gmail.com>
2026-02-08 01:07:01 +0500

    add advantages and reviews sections with responsive layout

---
+++ app/components/section/advantages.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+
+type Advantage = {
+	message: {
+		color: string,
+		text: string
+	}[],
+	description: string,
+	lost?: boolean
+}
+
+const advantages = <Advantage[]> [
+	{
+		message: [
+			{
+				color: '#FFCC00',
+				text: 'Высокий стаж'
+			},
+			{
+				color: '#FFFFFF',
+				text: 'водителей'
+			}
+		],
+		description: 'стаж наших водителей от 7 - 10 лет опыта'
+	},
+	{
+		message: [
+			{
+				color: '#FFFFFF',
+				text: 'скорость и'
+			},
+			{
+				color: '#FFCC00',
+				text: 'доступность'
+			}
+		],
+		description: 'работаем круглосуточно'
+	},
+	{
+		message: [
+			{
+				color: '#FFFFFF',
+				text: 'Безопасность и'
+			},
+			{
+				color: '#FFCC00',
+				text: 'гарантия'
+			}
+		],
+		description: 'предоставляем отчетные документы'
+	},
+	{
+		message: [
+			{
+				color: '#FFFFFF',
+				text: 'Перевозка и'
+			},
+			{
+				color: '#FFCC00',
+				text: 'доставка'
+			}
+		],
+		description: 'осуществляем перевозки и доставки посылок',
+		lost: true
+	}
+]
+</script>
+
+<template>
+	<section class="section-advantages w-full mx-auto h-screen">
+		<UContainer class="flex flex-col justify-center items-center gap-5">
+			<SectionHeader title="Наши преимущества" />
+			<div class="lg:w-[70%] flex flex-col justify-center items-center pt-10 gap-5">
+				<template v-for="(item, index) in advantages as Advantage[]" :key="index">
+					<div class="item flex flex-col justify-center items-center gap-1 w-full">
+						<div class="flex flex-row justify-center items-center gap-2 w-full text-white">
+							<span v-for="i in item.message" :class="`uppercase sm:text-[16px] md:text-[24px] lg:text-[40px] text-[${i.color}]`">{{ i.text	}}</span>
+						</div>
+						<div class="text-white sm:text-[14px] md:text-[16px] lg:text-[20px] uppercase">{{ item.description }}</div>
+					</div>
+					<img v-if="!item.lost" src="/images/svg/arrow-b.svg" alt="arrow-b" class="h-7"/>
+				</template>
+			</div>
+		</UContainer>
+	</section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/components/section/reviews.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+type Review = {
+	name: string,
+	text: string,
+	rating: number,
+	date: string
+}
+
+const reviews = <Review[]> [{
+	name: 'Иванов Иван',
+	text: 'Отличная компания! Всегда вовремя и с комфортом. Рекомендую всем!',
+	rating: 5,
+	date: '12.08.2024'
+}, {
+	name: 'Петров Петр',
+	text: 'Очень доволен сервисом. Водители вежливые и профессиональные.',
+	rating: 5,
+	date: '05.09.2024'
+}, {
+	name: 'Сидоров Сидор',
+	text: 'Хорошая компания, но иногда бывают задержки. В целом, рекомендую.',
+	rating: 4,
+	date: '05.09.2024'
+}]
+</script>
+
+<template>
+	<section class="section-reviews w-full mx-auto h-screen">
+		<UContainer class="flex flex-col justify-center items-center gap-5">
+			<SectionHeader title="Отзывы" />
+			<div class=" w-full flex flex-col justify-start items-center pt-10 gap-10">
+				<template v-for="(item, index) in reviews as Review[]" :key="index">
+					<div class="item flex flex-col justify-start items-start gap-1 w-full">
+						<div class="flex flex-row justify-between items-start gap-2 w-full">
+							<div class="flex flex-row justify-center items-center gap-5 text-gray-600">
+								<span class="uppercase text-[40px]">
+									<UIcon name="i-lucide-circle-user-round" size="32" />
+									{{ item.name }}
+								</span>
+								<div class="flex flex-row justify-start items-start gap-1">
+									<img height="20" width="20" v-for="n in item.rating" src="/images/svg/star.svg" :alt="`star +${n}`" />
+								</div>
+							</div>
+							<div class="flex flex-row justify-center items-center gap-1 text-gray-600">
+								<UIcon name="i-lucide-calendar" size="14" />
+								<p>{{ item.date }}</p>
+							</div>
+						</div>
+						<div class="text-gray-600 text-[20px] uppercase">{{ item.text }}</div>
+					</div>
+				</template>
+				<div class="w-full flex flex-row justify-between items-center pt-10 gap-5">
+					<UButton
+							class="button-gradient uppercase"
+							icon="i-lucide-ellipsis"
+					>
+						еще
+					</UButton>
+					<UButton
+							class="button-gradient uppercase"
+							icon="i-lucide-plus"
+					>
+						оставить отзыв
+					</UButton>
+				</div>
+			</div>
+		</UContainer>
+	</section>
+</template>
+
+<style scoped>
+
+</style>
