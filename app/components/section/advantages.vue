abc003ff849dce00bcef3faae4304aa160d04547
magixe <emax.mails@gmail.com>
2026-02-08 03:14:18 +0500

    refactor advantages and reviews components for improved layout and styling; enhance responsiveness and code formatting

--- app/components/section/advantages.vue
+++ app/components/section/advantages.vue
@@ -9,7 +9,7 @@ type Advantage = {
 	lost?: boolean
 }
 
-const advantages = <Advantage[]> [
+const advantages = <Advantage[]>[
 	{
 		message: [
 			{
@@ -69,14 +69,19 @@ const advantages = <Advantage[]> [
 <template>
 	<section class="section-advantages w-full mx-auto h-screen">
 		<UContainer class="flex flex-col justify-center items-center gap-5">
-			<SectionTitle title="Наши преимущества" />
+			<SectionTitle title="Наши преимущества"/>
 			<div class="lg:w-[70%] flex flex-col justify-center items-center pt-10 gap-5">
 				<template v-for="(item, index) in advantages as Advantage[]" :key="index">
 					<div class="item flex flex-col justify-center items-center gap-1 w-full">
 						<div class="flex flex-row justify-center items-center gap-2 w-full text-white">
-							<span v-for="i in item.message" :class="`uppercase sm:text-[16px] md:text-[24px] lg:text-[40px] text-[${i.color}]`">{{ i.text	}}</span>
+							<span v-for="i in item.message"
+							      :class="`uppercase sm:text-[16px] md:text-[24px] lg:text-[40px] text-[${i.color}]`">{{
+									i.text
+								}}</span>
 						</div>
-						<div class="text-white sm:text-[14px] md:text-[16px] lg:text-[20px] uppercase">{{ item.description }}</div>
+						<div class="text-white sm:text-[10px] md:text-[14px] lg:text-[20px] w-full text-center uppercase">
+							{{ item.description }}
+						</div>
 					</div>
 					<img v-if="!item.lost" src="/images/svg/arrow-b.svg" alt="arrow-b" class="h-7"/>
 				</template>
