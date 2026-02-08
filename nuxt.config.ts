ca36fafc7a5aa69231b89e737c8a1cebbf7ab489
magixe <emax.mails@gmail.com>
2026-02-08 21:42:02 +0500

    add new favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['/assets/css/main.css'],
-  ui: {
-    colorMode: false
-  }
+	compatibilityDate: '2025-07-15',
+	devtools: {enabled: true},
+	modules: ['@nuxt/ui'],
+	css: ['/assets/css/main.css'],
+	ui: {
+		colorMode: false
+	},
+	app: {
+		head: {
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/x-icon',
+					href: '/favicon.ico'
+				}
+			]
+		}
+	}
 })
