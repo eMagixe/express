d636b03ecd412bb064087340377cd090d3ab46eb
magixe <emax.mails@gmail.com>
2026-02-22 13:42:57 +0500

    refactor logo component: wrap in NuxtLink pointing to external URL

--- app/components/logo.vue
+++ app/components/logo.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-	<picture>
-		<source media="(min-width: 573px)" srcset="/images/svg/logo.svg" />
-		<source media="(max-width: 573px)" srcset="/images/svg/sm-logo.svg" />
-		<img height="72" width="240" src="/images/svg/logo.svg" alt="logo" style="width: auto" />
-	</picture>
+	<NuxtLink to="https://taxi-express.su/">
+		<picture>
+			<source media="(min-width: 573px)" srcset="/images/svg/logo.svg" />
+			<source media="(max-width: 573px)" srcset="/images/svg/sm-logo.svg" />
+			<img height="72" width="240" src="/images/svg/logo.svg" alt="logo" style="width: auto" />
+		</picture>
+	</NuxtLink>
 </template>
 
 <style scoped></style>
